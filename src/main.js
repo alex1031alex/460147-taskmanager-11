e592bcfc9f43cc69025b2085d3fc3b5b0278bee0
Alex <alex1031alex@yandex.ru>
2020-04-03 21:33:54 +0300

    Исправляет отступы в шаблонных строках

--- src/main.js
+++ src/main.js
@@ -11,64 +11,64 @@ const render = (container, template, place = `beforeend`) => {
 
 const createFilterTemplate = () => (
   `<section class="main__filter filter container">
-    <input
-      type="radio"
-      id="filter__all"
-      class="filter__input visually-hidden"
-      name="filter"
-      checked
-    />
-    <label for="filter__all" class="filter__label">
-      All <span class="filter__all-count">13</span>
-    </label>
-    <input
-      type="radio"
-      id="filter__overdue"
-      class="filter__input visually-hidden"
-      name="filter"
-      disabled
-    />
-    <label for="filter__overdue" class="filter__label">
-      Overdue <span class="filter__overdue-count">0</span>
-    </label>
-    <input
-      type="radio"
-      id="filter__today"
-      class="filter__input visually-hidden"
-      name="filter"
-      disabled
-    />
-    <label for="filter__today" class="filter__label">
-      Today <span class="filter__today-count">0</span>
-    </label>
-    <input
-      type="radio"
-      id="filter__favorites"
-      class="filter__input visually-hidden"
-      name="filter"
-    />
-    <label for="filter__favorites" class="filter__label">
-      Favorites <span class="filter__favorites-count">1</span>
-    </label>
-    <input
-      type="radio"
-      id="filter__repeating"
-      class="filter__input visually-hidden"
-      name="filter"
-    />
-    <label for="filter__repeating" class="filter__label">
-      Repeating <span class="filter__repeating-count">1</span>
-    </label>
-    <input
-      type="radio"
-      id="filter__archive"
-      class="filter__input visually-hidden"
-      name="filter"
-    />
-    <label for="filter__archive" class="filter__label">
-      Archive <span class="filter__archive-count">115</span>
-    </label>
-  </section>`
+     <input
+       type="radio"
+       id="filter__all"
+       class="filter__input visually-hidden"
+       name="filter"
+       checked
+     />
+     <label for="filter__all" class="filter__label">
+       All <span class="filter__all-count">13</span>
+     </label>
+     <input
+       type="radio"
+       id="filter__overdue"
+       class="filter__input visually-hidden"
+       name="filter"
+       disabled
+     />
+     <label for="filter__overdue" class="filter__label">
+       Overdue <span class="filter__overdue-count">0</span>
+     </label>
+     <input
+       type="radio"
+       id="filter__today"
+       class="filter__input visually-hidden"
+       name="filter"
+       disabled
+     />
+     <label for="filter__today" class="filter__label">
+       Today <span class="filter__today-count">0</span>
+     </label>
+     <input
+       type="radio"
+       id="filter__favorites"
+       class="filter__input visually-hidden"
+       name="filter"
+     />
+     <label for="filter__favorites" class="filter__label">
+       Favorites <span class="filter__favorites-count">1</span>
+     </label>
+     <input
+       type="radio"
+       id="filter__repeating"
+       class="filter__input visually-hidden"
+       name="filter"
+     />
+     <label for="filter__repeating" class="filter__label">
+       Repeating <span class="filter__repeating-count">1</span>
+     </label>
+     <input
+       type="radio"
+       id="filter__archive"
+       class="filter__input visually-hidden"
+       name="filter"
+     />
+     <label for="filter__archive" class="filter__label">
+       Archive <span class="filter__archive-count">115</span>
+     </label>
+   </section>`
 );
 
 const createBoardTemplate = () => (
@@ -291,48 +291,48 @@ const createEditTaskTemplate = () => (
 
 const createTaskTemplate = () => (
   `<article class="card card--black">
-    <div class="card__form">
-      <div class="card__inner">
-        <div class="card__control">
-          <button type="button" class="card__btn card__btn--edit">
-            edit
-          </button>
-          <button type="button" class="card__btn card__btn--archive">
-            archive
-          </button>
-          <button
-            type="button"
-            class="card__btn card__btn--favorites card__btn--disabled"
-          >
-            favorites
-          </button>
-        </div>
+     <div class="card__form">
+       <div class="card__inner">
+         <div class="card__control">
+           <button type="button" class="card__btn card__btn--edit">
+             edit
+           </button>
+           <button type="button" class="card__btn card__btn--archive">
+             archive
+           </button>
+           <button
+             type="button"
+             class="card__btn card__btn--favorites card__btn--disabled"
+           >
+             favorites
+           </button>
+         </div>
 
-        <div class="card__color-bar">
-          <svg class="card__color-bar-wave" width="100%" height="10">
-            <use xlink:href="#wave"></use>
-          </svg>
-        </div>
+         <div class="card__color-bar">
+           <svg class="card__color-bar-wave" width="100%" height="10">
+             <use xlink:href="#wave"></use>
+           </svg>
+         </div>
 
-        <div class="card__textarea-wrap">
-          <p class="card__text">Example task with default color.</p>
-        </div>
+         <div class="card__textarea-wrap">
+           <p class="card__text">Example task with default color.</p>
+         </div>
 
-        <div class="card__settings">
-          <div class="card__details">
-            <div class="card__dates">
-              <div class="card__date-deadline">
-                <p class="card__input-deadline-wrap">
-                  <span class="card__date">23 September</span>
-                  <span class="card__time">16:15</span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </article>`
+         <div class="card__settings">
+           <div class="card__details">
+             <div class="card__dates">
+               <div class="card__date-deadline">
+                 <p class="card__input-deadline-wrap">
+                   <span class="card__date">23 September</span>
+                   <span class="card__time">16:15</span>
+                 </p>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </article>`
 );
 
 const creatLoadMoreButtonTemplate = () => (
