63fef89426d325360dc0aa43f25e74826506797c
Alex <alex1031alex@yandex.ru>
2020-04-09 19:17:46 +0300

    Добавляет модуль для хранения констант

---
+++ src/const.js
@@ -0,0 +1,29 @@
+const FILTER_NAMES = [
+  `all`,
+  `overdue`,
+  `today`,
+  `favorits`,
+  `repeating`,
+  `archive`
+];
+
+const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export { MONTH_NAMES, COLORS, DAYS, FILTER_NAMES };
