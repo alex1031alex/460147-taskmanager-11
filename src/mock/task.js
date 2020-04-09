e666881c49b57f2305b240c0f85ce6517385a644
Alex <alex1031alex@yandex.ru>
2020-04-09 19:20:10 +0300

    Создаёт функции для генерации случайных данных для задач

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,11 +1,62 @@
+import { getRandomInteger } from './random.js';
+import { COLORS } from './../const.js';
+
+const DefaultRepeatingDays = {
+  "mo": false,
+  "tu": false,
+  "we": false,
+  "th": false,
+  "fr": false,
+  "sa": false,
+  "su": false,
+};
+
+const descriptionItems = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив`,
+  `Сделать уборку`,
+  `Отремонтировать велосипед`,
+  `Купить мебель`,
+  `Повесить полку`,
+  `Завести кота`,
+  `Сбросить 10 кг`,
+  `Взойти на Килиманджаро`,
+  `Заработать миллион`,
+];
+
+const getRandomArrayItem = (array) => {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = getRandomInteger(0, 1) < 1 ? -1 : 1;
+  const diffValue = sign * getRandomInteger(1, 7);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
+};
+
 const generateTask = () => {
-  return {};
+  const dueDate = getRandomInteger(1, 0) < 1 ? null : getRandomDate();
+
+  return {
+    description: getRandomArrayItem(descriptionItems),
+    dueDate,
+    repeatDays: Object.assign({}, DefaultRepeatingDays,
+      { "mo": Math.random() > 0.5 }),
+    color: getRandomArrayItem(COLORS),
+    isArchive: getRandomInteger(0, 1) < 1,
+    isFavorite: getRandomInteger(0, 1) < 1,
+  };
 };
 
 const generateTasks = (count) => {
   return new Array(count)
     .fill(``)
     .map(generateTask);
- };
+};
 
 export { generateTask, generateTasks };
