33561fbf26d51c37b9fb523c54121a99dcb15ba6
Alex <alex1031alex@yandex.ru>
2020-06-07 22:40:04 +0300

    Сохранит контейнеры в переменные

--- src/main.js
+++ src/main.js
@@ -373,5 +373,7 @@ const createLoadMoreButtonTemplate = () => {
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardElement = siteMainElement.querySelector(`.board`);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
