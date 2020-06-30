3daaa9b5b0a349eba91f47b152e6441a8126bd7b
Alex <alex1031alex@yandex.ru>
2020-06-30 16:09:41 +0300

    4.1.9 Реализует отрисовку доски, перенесёт логику LOAD MORE.WIP

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ import {generateTasks} from './mock/task.js';
 import {generateFilters} from './mock/filter.js';
 import {render, RenderPosition} from "./utils.js";
 
-
+// Установим количество задач для отрисовки
 const TASK_COUNT = 22;
 const SHOWING_TASK_ON_START = 8;
-const SHOWING_TASK_ON_BUTTON = 8;
-
+const SHOWING_TASK_BY_BUTTON = 8;
+// Определим функцию для отрисовки задачи
 const renderTask = (taskListElement, task) => {
   const onEditButtonClick = () => {
     taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
@@ -22,27 +22,54 @@ const renderTask = (taskListElement, task) => {
 
   const onEditFormSubmit = (evt) => {
     evt.preventDefault();
-    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement())
+    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
   };
+  // Создадим компонент, найдём кнопку, навесим обработчик
+  const taskComponent = new TaskComponent(task);
+  const editButton = taskComponent.getElement().querySelector(`form`);
+  editButton.addEventListener(`click`, onEditButtonClick);
 
-const taskComponent = new TaskComponent(task);
-const editButton = taskComponent.getElement().querySelector(`form`);
-editButton.addEventListener(`click`, onEditButtonClick);
-
-const taskEditComponent = new TaskEditComponent(task);
-const editForm = taskEditComponent.getElement().querySelector(`form`);
-editForm.addEventListener(`submit`, onEditButtonClick);
-
-render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
+  const taskEditComponent = new TaskEditComponent(task);
+  const editForm = taskEditComponent.getElement().querySelector(`form`);
+  editForm.addEventListener(`submit`, onEditFormSubmit);
+  // Отрисуем задачу
+  render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
 };
-
-const renderBoard = () => {};
-
+// Создадим функцию отрисовки доски
+const renderBoard = (boardComponent, tasks) => {
+  render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
+  // Найдём элемент списка задач
+  const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
+  // Отрисуем начальное количество задач в список
+  let showingTasksCount = SHOWING_TASK_ON_START;
+  tasks.slice(0, showingTasksCount)
+    .forEach((task) => {
+      renderTask(taskListElement, task);
+    });
+  // Отрисуем кнопку load-more
+  const loadMoreButtonComponent = new LoadMoreButtonComponent();
+  render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+  // Навесим обработчик на кнопку
+  loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
+    const prevTasksCount = showingTaskCount;
+    showingTasksCount = showingTasksCount + SHOWING_TASK_BY_BUTTON;
+    // По нажатию кнопки отрисовываем новое количество задач
+    tasks.slice(prevTasksCount, showingTasksCount)
+      .forEach((task) => renderTask(taskListElement, task));
+    // Проверяем не пора ли скрыть кнопку
+    if (showingTasksCount >= tasks.length) {
+      loadMoreButtonComponent.getElement().remove();
+      loadMoreButtonComponent.removeElement();
+    }
+  });
+};
+// Найдём и сохраним основные элементы страницы
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
+// Получим моковые данные
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
-
+// Отрисуем меню и фильтры
 render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
