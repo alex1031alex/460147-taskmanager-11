7fa5c2ac4b0018c6f15f6d2f87ded20105bb45eb
Alex <alex1031alex@yandex.ru>
2020-07-27 21:08:02 +0300

    6.1.7 Реализует функцию обновления данных и перерисовку компонента. Исправляет смену карточки на карточку редактирования и обратно

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,8 +3,9 @@ import TaskEditComponent from '../components/task-edit.js';
 import {render, replace, RenderPosition} from '../utils/render.js';
 
 export default class TaskController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
 
     this._taskComponent = null;
     this._taskEditComponent = null;
@@ -22,9 +23,15 @@ export default class TaskController {
     });
 
     this._taskComponent.setArhciveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
     });
 
     this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
     });
 
     this._taskEditComponent.setSubmitHandler((evt) => {
@@ -36,11 +43,11 @@ export default class TaskController {
   }
 
   _replaceEditToTask() {
-    replace(this._taskEditComponent, this._taskComponent);
+    replace(this._taskComponent, this._taskEditComponent);
   }
 
   _replaceTaskToEdit() {
-    replace(this._taskComponent, this._taskEditComponent);
+    replace(this._taskEditComponent, this._taskComponent);
   }
 
   _onEscKeyDown(evt) {
